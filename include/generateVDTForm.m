3085ffa4df6d8234491374f5529abb8b1412817b
lying2020 <824262075@qq.com>
2023-09-18 13:09:33 +0800

    update log

--- include/generateVDTForm.m
+++ include/generateVDTForm.m
@@ -29,12 +29,12 @@ displaytimelog(['func: ', func_name, '. ' 'nCores: ', num2str(nCores), ', [num_x
 numx = num_xyz(1); numy = num_xyz(2);  numz = num_xyz(3);
 
 tempVDTForm = cell(numx, numy, numz);
-% for ix = 1:numx
-parfor ix = 1:numx
+for ix = 1:numx
+% parfor ix = 1:numx
     tmpVDTForm_X = cell(numy, numz);
     X_time_start = str2num(showtimenow(0));
-     for iy = 1:numy
-    % parfor iy = 1:numy
+    %  for iy = 1:numy
+    parfor iy = 1:numy
         for iz = 1:numz
              tic
              sourceLocationCoord = coordBegin + retrieval_model_grid_size' .* [ix-1, iy-1, iz-1];
